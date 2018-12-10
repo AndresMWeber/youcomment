f6de520710c0ec6839eccecbc11f98458b28b38f
andresmweber <andresmweber@gmail.com>
2018-12-10 01:41:46 -0500

    Disabled loggers BEFORE initializing my logger.

--- youcomment/youlog.py
+++ youcomment/youlog.py
@@ -2,13 +2,11 @@ import logging
 from six import iteritems
 from youcomment.conf import __here__, LOG_PATH
 
-log = logging.getLogger(__here__)
-
-logging.basicConfig(level=logging.INFO)
-
 for name, logger in iteritems(logging.root.manager.loggerDict):
-    logger.disabled=True
+    logger.disabled = True
 
+log = logging.getLogger(__here__)
+logging.basicConfig(level=logging.INFO)
 logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s][%(name)s] [%(levelname)-5.5s] - %(message)s")
 
 fileHandler = logging.FileHandler(LOG_PATH)
