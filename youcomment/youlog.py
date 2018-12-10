9a7557a2cc7b865624a867bca53aed425c2651e5
andresmweber <andresmweber@gmail.com>
2018-12-10 00:36:00 -0500

    Quieting other loggers.

--- youcomment/youlog.py
+++ youcomment/youlog.py
@@ -1,7 +1,11 @@
 import logging
+from six import iteritems
 from youcomment.conf import __here__, LOG_PATH
 
 logging.basicConfig(level=logging.INFO)
+
+for name, logger in iteritems(logging.root.manager.loggerDict):
+    logger.disabled=True
 
 logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s][%(name)s] [%(levelname)-5.5s] - %(message)s")
 log = logging.getLogger(__here__)
