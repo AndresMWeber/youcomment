cc2d8c77acca25e50926330e3639b612b17dd8e3
andresmweber <andresmweber@gmail.com>
2018-12-10 01:25:38 -0500

    Pre sorting results for test comparing.

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -9,9 +9,8 @@ class TestBot(BaseTest):
     def test_default_run(self):
         bot = YouCompareBot(subreddits=TEST_SUBREDDIT)
         bot.run()
-        self.assertEqual(
-            [c.similarity for c in CrossCommentRelationship.select()],
-            [0.7668393782383419, 1.0, 0.993006993006993, 0.8809523809523809])
+        self.assertEqual(sorted([c.similarity for c in CrossCommentRelationship.select()]),
+                         sorted([0.7668393782383419, 1.0, 0.993006993006993, 0.8809523809523809]))
         self.assertEqual(len(RedditPost.select()),
                          len(list(bot.reddit_bot.subreddit('+'.join([TEST_SUBREDDIT])).top(limit=None))))
 
@@ -19,9 +18,7 @@ class TestBot(BaseTest):
         bot = YouCompareBot(subreddits='test123456123456')
         bot.reddit_bot.REDDIT_MAX_POSTS = 0
         bot.run()
-        self.assertEqual(
-            len(CrossCommentRelationship.select()),
-            4)
+        self.assertEqual(len(CrossCommentRelationship.select()), 4)
         self.assertEqual(len(RedditPost.select()),
                          len(list(bot.reddit_bot.subreddit('+'.join([TEST_SUBREDDIT])).top(limit=None))))
 
@@ -29,7 +26,5 @@ class TestBot(BaseTest):
         bot = YouCompareBot(subreddits='test123456123456')
         bot.reddit_bot.REDDIT_MAX_POSTS = 1
         bot.run()
-        self.assertEqual(
-            len(CrossCommentRelationship.select()),
-            0)
+        self.assertEqual(len(CrossCommentRelationship.select()), 0)
         self.assertEqual(len(RedditPost.select()), 1)
